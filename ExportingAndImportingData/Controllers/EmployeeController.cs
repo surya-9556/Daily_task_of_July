fe2fe8b2107c397fd86ea06521158d0b4c969719
Lenovo <surya-9556>
2021-07-11 11:04:30 +0530

    Expoting and importing of data from excel to sql and vice-versa

---
+++ ExportingAndImportingData/Controllers/EmployeeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExportingAndImportingData.Models;
+using OfficeOpenXml;
+
+namespace ExportingAndImportingData.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private PredictionEntities PE = new PredictionEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = (from i in PE.Employees 
+                              select i).ToList();
+            return View(employees);
+        }
+
+        public void ExportingToExcel()
+        {
+            var employees = (from i in PE.Employees
+                             select i).ToList();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ExcelPackage pck = new ExcelPackage();
+            ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Report");
+
+            ws.Cells["A1"].Value = "EmployeeName";
+            ws.Cells["B1"].Value = "BirthDate";
+            ws.Cells["C1"].Value = "HireDate";
+            ws.Cells["D1"].Value = "Gender";
+            ws.Cells["E1"].Value = "MaritalStatus";
+            ws.Cells["F1"].Value = "Email";
+            ws.Cells["G1"].Value = "PhoneNumber";
+
+            int rowStart = 2;
+            foreach (var item in employees)
+            {
+                ws.Cells[string.Format("A{0}", rowStart)].Value = item.EmployeeName;
+                ws.Cells[string.Format("B{0}", rowStart)].Value = item.BirthDate;
+                ws.Cells[string.Format("C{0}", rowStart)].Value = item.HireDate;
+                ws.Cells[string.Format("D{0}", rowStart)].Value = item.Gender;
+                ws.Cells[string.Format("E{0}", rowStart)].Value = item.MaritalStatus;
+                ws.Cells[string.Format("F{0}", rowStart)].Value = item.Email;
+                ws.Cells[string.Format("G{0}", rowStart)].Value = item.PhoneNumber;
+            }
+
+            ws.Cells["A:AZ"].AutoFitColumns();
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
+            Response.BinaryWrite(pck.GetAsByteArray());
+            Response.End();
+        }
+
+        public ActionResult Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //Alternative method for exporting the data
+        public ActionResult GetEmp()
+        {
+            var Emp = (from i in PE.Employees
+                       select new
+                       {
+                           i.EmployeeName,
+                           i.BirthDate,
+                           i.HireDate,
+                           i.Gender,
+                           i.MaritalStatus,
+                           i.Email,
+                           i.PhoneNumber,
+                           i.EmpId
+                       }).ToList();
+            return Json(new { data = Emp }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ExportingAndImportingData/Controllers/ImportingController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExportingAndImportingData.Models;
+using OfficeOpenXml;
+
+namespace ExportingAndImportingData.Controllers
+{
+    public class ImportingController : Controller
+    {
+        [HttpPost]
+        public ActionResult Upload(FormCollection formCollection)
+        {
+            var LinkList = new List<Importing>();
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    using (var package = new ExcelPackage(file.InputStream))
+                    { 
+                        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            var Imp = new Importing();
+                            Imp.Sno = Convert.ToInt32(workSheet.Cells[rowIterator, 1].Value);
+                            Imp.Name = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            Imp.Link = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            LinkList.Add(Imp);
+                        }
+                    }
+                }
+            }
+            using (masterEntities ME = new masterEntities())
+            {
+                foreach (var item in LinkList)
+                {
+                    ME.Importings.Add(item);
+                }
+                ME.SaveChanges();
+            }
+            return View("Import");
+        }
+
+        public ActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetLink()
+        {
+            using (masterEntities ME = new masterEntities())
+            {
+                var link = (from i in ME.Importings
+                            select new
+                            {
+                                Sno = i.Sno,
+                                Name = i.Name,
+                                Link = i.Link
+                            }).ToList();
+                return Json(new { data = link }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
